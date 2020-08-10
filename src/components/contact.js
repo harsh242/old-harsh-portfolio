c5ebf81b20a70692e695810c6087272a60083860
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2020-08-10 20:15:13 +0530

    about updated and seo tags

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ function Contact() {
       <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-col text-center w-full mb-12">
           <h1 class="sm:text-5xl text-2xl font-medium title-font mb-4 text-gray-900">Let's create experiences together</h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-lg"> I am Open for all types of Design and Development Projects <br/> If you want to collab or talk <br/> Mail me at : <a class="text-indigo-500" href="mailto:harshbadhai242@gmail.com">harshbadhai242@gmail.com</a></p>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-lg"> I am Open for all types of <span className="font-medium text-indigo-500">Design, Animation and Development </span> Projects <br/> If you want to collab or just talk <br/> Mail me at : <a class="text-indigo-500" href="mailto:harshbadhai242@gmail.com">harshbadhai242@gmail.com</a></p>
         </div>
       </div>
     </section>
