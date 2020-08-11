0ff135520b42fe79f6d3620fe93920ee8c0e79dc
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2020-08-11 13:23:39 +0530

    flexbox & optimized for safari

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ function About() {
         <div id="about">
           <section class="text-gray-700 body-font">
   <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap w-full mb-20">
+    <div class="flex flex-wrap w-full mb-20 items-start">
       <div class="lg:w-1/2 w-full  mb-6 lg:mb-0 pr-3">
         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">About Me</h1>
         <div class="h-1 w-20 bg-indigo-500 rounded"> </div>
